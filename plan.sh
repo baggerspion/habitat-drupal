fc97100a6ed57246d8fee92353987ca42c6a3a84
Paul Adams <paul@baggerspion.net>
2017-05-22 23:22:24 -0500

    Simplified plan

--- plan.sh
+++ plan.sh
@@ -2,8 +2,6 @@ pkg_name=drupal
 pkg_origin=baggerspion
 pkg_version="8.3.2"
 pkg_license=('gplv2+')
-pkg_source="N/A"
-pkg_shasum="N/A"
 pkg_deps=(core/mysql-client baggerspion/drush core/nginx core/php)
 pkg_binds=(
   [database]="port username password"
@@ -13,22 +11,10 @@ pkg_svc_run="php-fpm --nodaemonize"
 pkg_description="Drupal is a free and open source content-management framework written in PHP."
 pkg_upstream_url="https://www.drupal.org"
 
-do_download() {
-    return 0
-}
-
-do_verify() {
-    return 0
-}
-
-do_unpack() {
-    return 0
-}
-
 do_build() {
-    drush dl drupal-8.3.2 --drupal-project-rename=drupal
+  drush dl drupal-8.3.2 --destination="$CACHE_PATH" --drupal-project-rename=drupal
 }
 
 do_install() {
-    cp -r $HAB_CACHE_SRC_PATH/$pkg_dirname/drupal $pkg_prefix/drupal
+  cp -r "$CACHE_PATH/drupal" "$pkg_prefix/drupal"
 }
